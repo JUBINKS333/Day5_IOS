5243940f1a863d835808b3da324b7ee05587d51a
MacStudent <macstudent@126-imac21.local>
2019-03-08 19:50:28 -0500

    Picker

--- Day5_IOS/ViewController.swift
+++ Day5_IOS/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    
+    var countryList = ["INDIA", "CANADA", "USA", "CHINA", "RUSIA", "BRAZIL", "UNITED KINGDOM", "UAE", "DUBAI", "AUSTRALIA"]
+    var cityList = ["Mumbai", "Delhi", "Hyderabad", "Ahmedabad", "Surat", "Banglore"]
+    
+    @IBOutlet weak var pickerCountry: UIPickerView!
+    @IBOutlet weak var lblCountryName: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        self.pickerCountry.delegate = self
+        self.pickerCountry.dataSource = self
+        
     }
 
-
+    @IBAction func birthDatePicker(_ sender: UIDatePicker) {
+        print(sender.date)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0{
+            return self.countryList.count
+        }
+       
+            return self.cityList.count
+        
+      
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //return "Comp: \(component) - Row: \(row)"
+        if component == 0{
+            return self.countryList[row]
+        }
+       
+            return self.cityList[row]
+        
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        self.lblCountryName.text = "Country: \(self.countryList[self.pickerCountry.selectedRow(inComponent: 0)]), City: \(self.cityList[self.pickerCountry.selectedRow(inComponent: 1)])"
+        
+        
+    }
 }
 
